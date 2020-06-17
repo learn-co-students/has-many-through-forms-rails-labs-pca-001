d166d868e1c203cadf7a50e5126c63c2bf94e887
Jo Fischer <josephinefischer9@gmail.com>
2020-06-17 14:41:58 -0400

    done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     redirect_to comment.post
   end
 
-  private
+private
 
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
